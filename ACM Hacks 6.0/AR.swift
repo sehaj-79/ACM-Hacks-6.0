0942acfca86876be6bb0f69e5c8076834cbab9d6
Shikhar Kumar <shikharkumar009@gmail.com>
2022-02-11 12:43:48 +0530

    AR File

---
+++ ACM Hacks 6.0/AR.swift
@@ -0,0 +1,20 @@
+//
+//  AR.swift
+//  ACM Hacks 6.0
+//
+//  Created by Shikhar Kumar on 11/02/22.
+//
+
+import SwiftUI
+
+struct AR: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AR_Previews: PreviewProvider {
+    static var previews: some View {
+        AR()
+    }
+}

27a7c098688759d222b534707239cfd4c1fe68c9
sehaj-79 <bhasin.seh79@gmail.com>
2022-02-11 12:02:44 +0530

    Initial Commit

---
+++ ACM Hacks 6.0/ACM_Hacks_6_0App.swift
@@ -0,0 +1,17 @@
+//
+//  ACM_Hacks_6_0App.swift
+//  ACM Hacks 6.0
+//
+//  Created by sehejbir bhasin on 11/02/22.
+//
+
+import SwiftUI
+
+@main
+struct ACM_Hacks_6_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ACM Hacks 6.0/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ACM Hacks 6.0
+//
+//  Created by sehejbir bhasin on 11/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
